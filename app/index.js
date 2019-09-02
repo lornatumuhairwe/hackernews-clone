6644018c8afc0d0b5fdb750707d7bf78b488c26c
Lorna Tumuhairwe <lornatumuhairwe@gmail.com>
2019-09-02 08:40:00 +0300

    Setup with webpack, babel

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        Hello, Lorna!
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
